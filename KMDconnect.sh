c1f0a68ea7ef6457658bae71316ae71ed43602eb
gcharang <mrgcharang@gmail.com>
2019-07-22 16:55:55 +0530

    stops curl from failing silently

--- KMDconnect.sh
+++ KMDconnect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source userpass
-curl -s --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"electrum\",\"coin\":\"KMD\",\"servers\":[{\"url\":\"electrum1.cipig.net:10001\"},{\"url\":\"electrum2.cipig.net:10001\"}]}"
+curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"electrum\",\"coin\":\"KMD\",\"servers\":[{\"url\":\"electrum1.cipig.net:10001\"},{\"url\":\"electrum2.cipig.net:10001\"}]}"
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source userpass
-curl -s --url "http://127.0.0.1:7783" --data "{\"method\":\"stop\",\"userpass\":\"$userpass\"}"
+curl --url "http://127.0.0.1:7783" --data "{\"method\":\"stop\",\"userpass\":\"$userpass\"}"
