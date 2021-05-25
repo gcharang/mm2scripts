70624842ff5cf05ab39beb9f0a95cd30eb7b617a
phideas <phideas1@protonmail.com>
2021-05-25 09:11:55 +0000

    Added table view of MyBalance for all enabled coins

---
+++ mybalance_tab.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source userpass
+ 
+# Simplified table view of the enabled coins 
+# ---------------------------------------------------
+
+ENABLED_COINS=$(curl -Ss --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"get_enabled_coins\"}");
+
+for k in $(jq '.result | keys | .[]' <<< "$ENABLED_COINS"); do
+  
+  ticker=$(jq -r ".result[$k] | .ticker" <<< "$ENABLED_COINS");
+  mybalance=$(curl -Ss --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"my_balance\",\"coin\":\"$ticker\"}");
+  ENABLED_COINS=$(jq -r ".result[$k].mybalance |= . + [$mybalance]" <<< "$ENABLED_COINS");  
+
+done
+
+# printf "$ENABLED_COINS";
+
+printf "\n";
+printf "Coin    Balance     Unspendable Address\n";
+printf "%s----------------------------------------------------------------------\n";
+printf "$ENABLED_COINS" | jq -r '.result | sort_by(.ticker) | .[] | .mybalance | .[] | flatten|@tsv';
+printf "\n";
+ 
